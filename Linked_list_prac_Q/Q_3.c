780c772f1b5706d23676b4cc098f5aa3b5f4ced2
Arihant8005 <arihantrathore8005@gmail.com>
2026-06-09 17:30:41 +0530

    Added Q_3 in linked_list folder

---
+++ Linked_list_prac_Q/Q_3.c
@@ -0,0 +1,55 @@
+// create a linked list where user enter 5 numbers and display them
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+int main() {
+    struct Node *head = NULL;
+    struct Node *temp = NULL;
+    struct Node *newNode = NULL;
+
+    int i;
+
+    printf("Enter 5 numbers:\n");
+
+    for (i = 1; i <= 5; i++) {
+
+        // Create a new node
+        newNode = (struct Node *)malloc(sizeof(struct Node));
+
+        // Take input from user
+        scanf("%d", &newNode->data);
+
+        // Last node points to NULL
+        newNode->next = NULL;
+
+        // First node
+        if (head == NULL) {
+            head = newNode;
+            temp = head;
+        }
+        // Remaining nodes
+        else {
+            temp->next = newNode;
+            temp = newNode;
+        }
+    }
+
+    // Display the linked list
+    printf("\nLinked List: ");
+
+    temp = head;
+
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+
+    printf("NULL");
+
+    return 0;
+}
