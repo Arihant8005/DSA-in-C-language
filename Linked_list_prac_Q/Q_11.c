3dd92726d9bb7ca31413f5b8b177dbf10e1498a4
Arihant8005 <arihantrathore8005@gmail.com>
2026-06-11 16:25:14 +0530

    Added Q_11 in linked list folder

---
+++ Linked_list_prac_Q/Q_11.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *link;
+};
+
+int main() {
+
+    struct node *head = NULL;
+    struct node *temp = NULL;
+    struct node *new_node = NULL;
+
+    int n;
+
+    printf("Enter number of nodes: ");
+    scanf("%d", &n);
+    printf("Enter data: ");
+
+    for(int i = 0; i < n; i++) {
+
+        new_node = (struct node *)malloc(sizeof(struct node));
+
+        scanf("%d", &new_node->data);
+
+        new_node->link = NULL;
+
+        if(head == NULL) {
+            head = new_node;
+            temp = head;
+        }
+        else {
+            temp->link = new_node;
+            temp = new_node;
+        }
+    }
+
+    // Insert at beginning
+    new_node = (struct node *)malloc(sizeof(struct node));
+
+    printf("Enter data to insert at beginning: ");
+    scanf("%d", &new_node->data);
+
+    new_node->link = head;
+    head = new_node;
+    
+    temp = head;
+
+    printf("Linked List: ");
+
+    while(temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->link;
+    }
+
+    printf("NULL");
+}
