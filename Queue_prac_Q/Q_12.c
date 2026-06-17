abc7df6befd68017dd215b9c7c6b3fe7b0153419
Arihant8005 <arihantrathore8005@gmail.com>
2026-06-17 19:03:13 +0530

    Added Q_12 in queue folder

---
+++ Queue_prac_Q/Q_12.c
@@ -0,0 +1,34 @@
+// Display elements of a queue using circular linked list
+
+#include <stdio.h>
+
+int queue[5] = {10, 20, 30};
+int front = 0, rear = 2;
+
+void display()
+{
+    if (front == -1)
+    {
+        printf("Queue is empty\n");
+    }
+    else
+    {
+        int i = front;
+
+        while (i != rear)
+        {
+            printf("%d ", queue[i]);
+            i = (i + 1) % 5;
+        }
+
+        printf("%d", queue[rear]);
+    }
+}
+
+int main()
+{
+    printf("Queue elements: ");
+    display();
+
+    return 0;
+}
