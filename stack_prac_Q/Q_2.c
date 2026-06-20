2e17bb4a22dfced990ed37fb23a55c0a6d5b7ee6
Arihant8005 <arihantrathore8005@gmail.com>
2026-06-20 22:23:33 +0530

    Added Q_2 in stack folder

---
+++ stack_prac_Q/Q_2.c
@@ -0,0 +1,57 @@
+//Push 5 Elements into a Stack and Display Them
+
+#include <stdio.h>
+
+#define SIZE 5
+
+int stack[SIZE];
+int top = -1;
+
+void push(int data)
+{
+    if(top == SIZE - 1)
+    {
+        printf("Stack Overflow\n");
+    }
+    else
+    {
+        top++;
+        stack[top] = data;
+    }
+}
+
+void display()
+{
+    int i;
+
+    if(top == -1)
+    {
+        printf("Stack is empty\n");
+    }
+    else
+    {
+        printf("Stack elements are:\n");
+
+        for(i = top; i >= 0; i--)
+        {
+            printf("%d\n", stack[i]);
+        }
+    }
+}
+
+int main()
+{
+    int i, data;
+
+    printf("Enter 5 elements:\n");
+
+    for(i = 0; i < SIZE; i++)
+    {
+        scanf("%d", &data);
+        push(data);
+    }
+
+    display();
+
+    return 0;
+}
