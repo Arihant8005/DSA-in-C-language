88955c328d945aeda18d130b0ed7b95fbeae31a7
Arihant8005 <arihantrathore8005@gmail.com>
2026-06-24 19:42:51 +0530

    Added Q_3 in binary tree folder

---
+++ BInary_tree_prac_Q/Q_3.c
@@ -0,0 +1,56 @@
+//Create a binary tree with 5 nodes and perform an Inorder Traversal (Left → Root → Right).
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+void inorder(struct node *root)
+{
+    if(root != NULL)
+    {
+        inorder(root->left);
+        printf("%d ", root->data);
+        inorder(root->right);
+    }
+}
+
+int main()
+{
+    struct node *root;
+
+    root = (struct node *)malloc(sizeof(struct node));
+
+    root->data = 10;
+
+    root->left = (struct node *)malloc(sizeof(struct node));
+    root->left->data = 20;
+
+    root->right = (struct node *)malloc(sizeof(struct node));
+    root->right->data = 30;
+
+    root->left->left = (struct node *)malloc(sizeof(struct node));
+    root->left->left->data = 40;
+
+    root->left->right = (struct node *)malloc(sizeof(struct node));
+    root->left->right->data = 50;
+
+    root->right->left = NULL;
+    root->right->right = NULL;
+
+    root->left->left->left = NULL;
+    root->left->left->right = NULL;
+
+    root->left->right->left = NULL;
+    root->left->right->right = NULL;
+
+    printf("Inorder Traversal: ");
+    inorder(root);
+
+    return 0;
+}
