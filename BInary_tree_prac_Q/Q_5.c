dd5563a3003ff074a43ad313f59b88f75fbaf2b4
Arihant8005 <arihantrathore8005@gmail.com>
2026-06-24 19:47:08 +0530

    Added Q_5 in binary tree foler

---
+++ BInary_tree_prac_Q/Q_5.c
@@ -0,0 +1,55 @@
+//Perform Postorder Traversal of a Binary Tree
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+void postorder(struct node *root)
+{
+    if(root != NULL)
+    {
+        postorder(root->left);
+        postorder(root->right);
+        printf("%d ", root->data);
+    }
+}
+
+int main()
+{
+    struct node *root;
+
+    root = (struct node *)malloc(sizeof(struct node));
+    root->data = 10;
+
+    root->left = (struct node *)malloc(sizeof(struct node));
+    root->left->data = 20;
+
+    root->right = (struct node *)malloc(sizeof(struct node));
+    root->right->data = 30;
+
+    root->left->left = (struct node *)malloc(sizeof(struct node));
+    root->left->left->data = 40;
+
+    root->left->right = (struct node *)malloc(sizeof(struct node));
+    root->left->right->data = 50;
+
+    root->right->left = NULL;
+    root->right->right = NULL;
+
+    root->left->left->left = NULL;
+    root->left->left->right = NULL;
+
+    root->left->right->left = NULL;
+    root->left->right->right = NULL;
+
+    printf("Postorder Traversal: ");
+    postorder(root);
+
+    return 0;
+}
