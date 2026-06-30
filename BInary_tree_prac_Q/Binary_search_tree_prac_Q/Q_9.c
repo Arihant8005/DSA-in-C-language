c35ef7a3373771d39585e74a08ee1eb207110b9b
Arihant8005 <arihantrathore8005@gmail.com>
2026-06-30 21:22:21 +0530

    Added Q_9 in BST

---
+++ BInary_tree_prac_Q/Binary_search_tree_prac_Q/Q_9.c
@@ -0,0 +1,76 @@
+//Write a C program to count the number of leaf nodes in a Binary Search Tree (BST).
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node *createNode(int data)
+{
+    struct node *newNode;
+
+    newNode = (struct node *)malloc(sizeof(struct node));
+
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+
+    return newNode;
+}
+
+struct node *insert(struct node *root, int data)
+{
+    if(root == NULL)
+    {
+        return createNode(data);
+    }
+
+    if(data < root->data)
+    {
+        root->left = insert(root->left, data);
+    }
+    else if(data > root->data)
+    {
+        root->right = insert(root->right, data);
+    }
+
+    return root;
+}
+
+int countLeafNodes(struct node *root)
+{
+    if(root == NULL)
+    {
+        return 0;
+    }
+
+    if(root->left == NULL && root->right == NULL)
+    {
+        return 1;
+    }
+
+    return countLeafNodes(root->left) +
+           countLeafNodes(root->right);
+}
+
+int main()
+{
+    struct node *root = NULL;
+
+    root = insert(root, 50);
+    insert(root, 30);
+    insert(root, 70);
+    insert(root, 20);
+    insert(root, 40);
+    insert(root, 60);
+    insert(root, 80);
+
+    printf("Number of leaf nodes = %d\n", countLeafNodes(root));
+
+    return 0;
+}
